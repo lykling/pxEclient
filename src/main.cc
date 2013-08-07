24766487f5db19a02ce37b0482cbe6c67da967ba
legendlee1314 <legendlee1314@gmail.com>
2013-08-07 16:07:58 +0800

    First Framework With GYP

---
+++ src/main.cc
@@ -0,0 +1,14 @@
+// Copyright (c) 2013 Legend, Lykling. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+
+#include <cstdio>
+
+#include "auth.h"
+#include "option.h"
+#include "packet.h"
+
+int main(int argc, char *argv[]) {
+    return 0;
+}
---
+++ src/option.h
@@ -0,0 +1,9 @@
+// Copyright (c) 2013 Legend, Lykling. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef PXECLIENT_SRC_OPTION_H
+#define PXECLIENT_SRC_OPTION_H
+
+
+#endif
